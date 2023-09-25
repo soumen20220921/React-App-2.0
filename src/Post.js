0383e743533eb7a62055f4d859424ac15df394e4
Soumen Singh <Soumensingh951@gmail.com>
2023-09-25 08:55:15 +0530

    texting

--- src/Post.js
+++ src/Post.js
@@ -63,6 +63,65 @@ const Post = () => {
           </div>
         </div>
     </div>
+    <div className='header'>
+            <h1>Founder</h1>
+  </div>
+<div className='post'>
+        <div className='member'>
+          <img src=".\images\Abhisek Tiwari.jpeg" alt="" />
+          <div className='des'> 
+          <h3>
+            Abhisek Tiwari
+          </h3>
+          <div className='social'>
+          <div className="footer-social--icons">
+          
+              <div>
+              <a rel="noreferrer"
+                  href="https://www.instagram.com/"
+                  target="_blank">
+                  <FaTwitter className="icons" />
+                </a>
+              </div>
+              <div>
+                <a rel="noreferrer"
+                  href="https://www.linkedin.com/company/clinical-ai-assistance"
+                  target="_blank">
+                  <FaLinkedin className="icons"  />
+                </a>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div className='member'>
+          <img src=".\images\Sarbajeet Tiwari.jpeg" alt="" />
+          <div className='des'> 
+          <h3>
+          Sarbajeet Tiwari
+          </h3>
+          <div className='social'>
+          <div className="footer-social--icons">
+          
+              <div>
+              <a rel="noreferrer"
+                  href="https://www.instagram.com/"
+                  target="_blank">
+                  <FaTwitter className="icons" />
+                </a>
+              </div>
+              <div>
+                <a rel="noreferrer"
+                  href="https://www.linkedin.com/in/abhisektiwari/"
+                  target="_blank">
+                  <FaLinkedin className="icons"  />
+                </a>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+    </div>
    </div>
   )
 }
