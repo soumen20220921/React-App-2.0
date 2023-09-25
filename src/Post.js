7af40a0f03fa7705072ad0e8ce80298c951ba221
Soumen Singh <Soumensingh951@gmail.com>
2023-09-25 10:56:25 +0530

    adding team

--- src/Post.js
+++ src/Post.js
@@ -1,128 +1,472 @@
 import React from 'react'
 import "./Post.css";
-import {   FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaLinkedin, FaTwitter } from "react-icons/fa";
 const Post = () => {
   return (
-   <div>
-  <div className='header'>
-            <h1>Founder</h1>
-  </div>
-<div className='post'>
+    <div>
+      <div className='header'>
+        <h1>Founder</h1>
+      </div>
+      <div className='post'>
         <div className='member'>
           <img src=".\images\Abhisek Tiwari.jpeg" alt="" />
-          <div className='des'> 
-          <h3>
-            Abhisek Tiwari
-          </h3>
-          <div className='social'>
-          <div className="footer-social--icons">
-          
-              <div>
-              <a rel="noreferrer"
-                  href="https://www.instagram.com/"
-                  target="_blank">
-                  <FaTwitter className="icons" />
-                </a>
-              </div>
-              <div>
-                <a rel="noreferrer"
-                  href="https://www.linkedin.com/company/clinical-ai-assistance"
-                  target="_blank">
-                  <FaLinkedin className="icons"  />
-                </a>
+          <div className='des'>
+            <h3>
+              Abhisek Tiwari
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/company/clinical-ai-assistance"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
           </div>
         </div>
         <div className='member'>
           <img src=".\images\Sarbajeet Tiwari.jpeg" alt="" />
-          <div className='des'> 
-          <h3>
-          Sarbajeet Tiwari
-          </h3>
-          <div className='social'>
-          <div className="footer-social--icons">
-          
-              <div>
-              <a rel="noreferrer"
-                  href="https://www.instagram.com/"
-                  target="_blank">
-                  <FaTwitter className="icons" />
-                </a>
-              </div>
-              <div>
-                <a rel="noreferrer"
-                  href="https://www.linkedin.com/in/abhisektiwari/"
-                  target="_blank">
-                  <FaLinkedin className="icons"  />
-                </a>
+          <div className='des'>
+            <h3>
+              Sarbajeet Tiwari
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/in/abhisektiwari/"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
           </div>
         </div>
-    </div>
-    <div className='header'>
-            <h1>Founder</h1>
-  </div>
-<div className='post'>
+      </div>
+      <div className='header'>
+        <h1>Mentor</h1>
+      </div>
+      <div className='post'>
         <div className='member'>
-          <img src=".\images\Abhisek Tiwari.jpeg" alt="" />
-          <div className='des'> 
-          <h3>
-            Abhisek Tiwari
-          </h3>
-          <div className='social'>
-          <div className="footer-social--icons">
-          
-              <div>
-              <a rel="noreferrer"
-                  href="https://www.instagram.com/"
-                  target="_blank">
-                  <FaTwitter className="icons" />
-                </a>
-              </div>
-              <div>
-                <a rel="noreferrer"
-                  href="https://www.linkedin.com/company/clinical-ai-assistance"
-                  target="_blank">
-                  <FaLinkedin className="icons"  />
-                </a>
+          <img src=".\images\sriparna saha.jpg" alt="" />
+          <div className='des'>
+            <h3>
+              Sriparna Saha
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
+        </div>
+        <div className='member'>
+          <img src="./images/Pushpak Bhattacharyya.jpeg" alt="" />
+          <div className='des'>
+            <h3>
+              Pushpak Bhattacharyya
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='member'>
+          <img src=".\images\Minakshi Dhar.jpeg" alt="" />
+          <div className='des'>
+            <h3>
+              Minakshi Dhar
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/company/clinical-ai-assistance"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='header'>
+        <h1> Research Team</h1>
+      </div>
+      <div className='post'>
+        <div className='member'>
+          <img src=".\images\Abhisek Tiwari.jpeg" alt="" />
+          <div className='des'>
+            <h3>
+              Abhisek Tiwari
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/company/clinical-ai-assistance"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className='member'>
           <img src=".\images\Sarbajeet Tiwari.jpeg" alt="" />
-          <div className='des'> 
-          <h3>
-          Sarbajeet Tiwari
-          </h3>
-          <div className='social'>
-          <div className="footer-social--icons">
-          
-              <div>
-              <a rel="noreferrer"
-                  href="https://www.instagram.com/"
-                  target="_blank">
-                  <FaTwitter className="icons" />
-                </a>
-              </div>
-              <div>
-                <a rel="noreferrer"
-                  href="https://www.linkedin.com/in/abhisektiwari/"
-                  target="_blank">
-                  <FaLinkedin className="icons"  />
-                </a>
+          <div className='des'>
+            <h3>
+              Sarbajeet Tiwari
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/in/abhisektiwari/"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className='header'>
+        <h1>Technical Team</h1>
+      </div>
+      <div className='post'>
+        <div className='member'>
+          <img src=".\images\Abhisek Tiwari.jpeg" alt="" />
+          <div className='des'>
+            <h3>
+              Abhisek Tiwari
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/company/clinical-ai-assistance"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+        <div className='member'>
+          <img src=".\images\Aryan Sahoo.jpeg" alt="" />
+          <div className='des'>
+            <h3>
+              Aryan Sahoo
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/company/clinical-ai-assistance"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='member'>
+          <img src=".\images\Soumen Singh.jpeg" alt="" />
+          <div className='des'>
+            <h3>
+              Soumen Singh
+
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/in/abhisektiwari/"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='post'>
+        <div className='member'>
+          <img src=".\images\varad Paswar.jpeg" alt="" />
+          <div className='des'>
+            <h3>
+            Varad Paswar
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/company/clinical-ai-assistance"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='member'>
+          <img src=".\images\karan.jpeg" alt="" />
+          <div className='des'>
+            <h3>
+            Karan Ravi Das
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/in/abhisektiwari/"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='header'>
+        <h1>Medical Team</h1>
+      </div>
+      <div className='post'>
+        <div className='member'>
+          <img src=".\images\Minakshi Dhar.jpeg" alt="" />
+          <div className='des'>
+            <h3>
+            Minakshi DharMinakshi Dhar
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/company/clinical-ai-assistance"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='member'>
+          <img src=".\images\Dr Pankaj.jpeg" alt="" />
+          <div className='des'>
+            <h3>
+            Dr Pankaj
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/company/clinical-ai-assistance"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='post'>
+        <div className='member'>
+          <img src=".\images\Sawarnin.jpeg" alt="" />
+          <div className='des'>
+            <h3>
+            Dr. Swarnim
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/company/clinical-ai-assistance"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='member'>
+          <img src=".\images\mrinmoy.jpeg" alt="" />
+          <div className='des'>
+            <h3>
+            Dr. Mrinmoy
+            </h3>
+            <div className='social'>
+              <div className="footer-social--icons">
+
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.instagram.com/"
+                    target="_blank">
+                    <FaTwitter className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a rel="noreferrer"
+                    href="https://www.linkedin.com/company/clinical-ai-assistance"
+                    target="_blank">
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-   </div>
   )
 }
 
